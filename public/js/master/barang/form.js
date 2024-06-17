057cb824eeab40aeb5b2b8f11ffeeb98bdc46491
bimaega15 <49235873+bimaega15@users.noreply.github.com>
2024-06-17 15:01:29 +0700

    Update Barang - Hidden autocomplete

--- public/js/master/barang/form.js
+++ public/js/master/barang/form.js
@@ -93,136 +93,136 @@ var isScroll = false;
 var isMaxPage = false;
 
 
-$('body').off('input', '.autocomplete');
-$('body').on('input', '.autocomplete', debounce(function (e) {
-    const value = $(this).val();
-    search = value;
-    let isSearch = false;
-    if (value != '') {
-        nextPage = 1;
-        listItem = [];
-        isSearch = true;
-        isScroll = false;
-    }
+// $('body').off('input', '.autocomplete');
+// $('body').on('input', '.autocomplete', debounce(function (e) {
+//     const value = $(this).val();
+//     search = value;
+//     let isSearch = false;
+//     if (value != '') {
+//         nextPage = 1;
+//         listItem = [];
+//         isSearch = true;
+//         isScroll = false;
+//     }
 
-    $.ajax({
-        url: `${baseurl}/autocomplete/barang`,
-        type: 'get',
-        dataType: 'json',
-        data: {
-            search: value,
-            page: nextPage,
-        },
-        success: function (data) {
-            const { count_filtered } = data;
-            const getDataAjax = (data) => {
-                const results = data.results;
-                const next_page_url = results.next_page_url !== null ? results.next_page_url.split('?page=')[1] : nextPage;
-                const newData = results.data;
-                isMaxPage = results.next_page_url !== null ? false : true;
+//     $.ajax({
+//         url: `${baseurl}/autocomplete/barang`,
+//         type: 'get',
+//         dataType: 'json',
+//         data: {
+//             search: value,
+//             page: nextPage,
+//         },
+//         success: function (data) {
+//             const { count_filtered } = data;
+//             const getDataAjax = (data) => {
+//                 const results = data.results;
+//                 const next_page_url = results.next_page_url !== null ? results.next_page_url.split('?page=')[1] : nextPage;
+//                 const newData = results.data;
+//                 isMaxPage = results.next_page_url !== null ? false : true;
 
-                if (newData.length > 0) {
-                    if (next_page_url !== nextPage && !isSearch) {
-                        listItem = listItem.concat(newData);
-                    } else if (isScroll) {
-                        listItem = listItem.concat(newData);
-                    } else {
-                        listItem = newData;
-                    }
-                }
+//                 if (newData.length > 0) {
+//                     if (next_page_url !== nextPage && !isSearch) {
+//                         listItem = listItem.concat(newData);
+//                     } else if (isScroll) {
+//                         listItem = listItem.concat(newData);
+//                     } else {
+//                         listItem = newData;
+//                     }
+//                 }
 
-                nextPage = next_page_url;
+//                 nextPage = next_page_url;
 
-                let listItemString = '';
-                let buttonCloseElement = '';
-                if (listItem.length > 0) {
-                    newData.forEach(value => {
-                        listItemString += `
-                        <li class="list-group-item d-flex justify-content-between align-items-start click-list-item" data-barcode_barang="${value.barcode_barang}" style="cursor: pointer;">
-                            <div class="ms-2 me-auto">
-                                <div class="fw-bold">${value.nama_barang}</div>
-                                    ${value.barcode_barang}
-                                </div>
-                        </li>
-                    `;
-                    });
+//                 let listItemString = '';
+//                 let buttonCloseElement = '';
+//                 if (listItem.length > 0) {
+//                     newData.forEach(value => {
+//                         listItemString += `
+//                         <li class="list-group-item d-flex justify-content-between align-items-start click-list-item" data-barcode_barang="${value.barcode_barang}" style="cursor: pointer;">
+//                             <div class="ms-2 me-auto">
+//                                 <div class="fw-bold">${value.nama_barang}</div>
+//                                     ${value.barcode_barang}
+//                                 </div>
+//                         </li>
+//                     `;
+//                     });
 
-                    buttonCloseElement = `
-                <button type="button" class="btn-close-autocomplete btn btn-md" style="
-                position: absolute;
-                z-index: 99;
-                padding: 0;
-                right: 23px;
-                bottom: 7px;
-            ">
-                    <i class="fa-regular fa-circle-xmark fa-1x" style="
-                    font-size: 20px;"></i>
-                </button>
-                `;
-                }
+//                     buttonCloseElement = `
+//                 <button type="button" class="btn-close-autocomplete btn btn-md" style="
+//                 position: absolute;
+//                 z-index: 99;
+//                 padding: 0;
+//                 right: 23px;
+//                 bottom: 7px;
+//             ">
+//                     <i class="fa-regular fa-circle-xmark fa-1x" style="
+//                     font-size: 20px;"></i>
+//                 </button>
+//                 `;
+//                 }
 
-                const inputElement = $('.autocomplete');
-                const listGroup = $('.list-group');
-                const btnCloseAutoComplete = $('.btn-close-autocomplete');
+//                 const inputElement = $('.autocomplete');
+//                 const listGroup = $('.list-group');
+//                 const btnCloseAutoComplete = $('.btn-close-autocomplete');
 
-                if (listGroup.length > 0) {
-                    if (isScroll) {
-                        listGroup.append(listItemString);
-                    } else {
-                        listGroup.html(listItemString);
-                    }
-                    btnCloseAutoComplete.replaceWith(buttonCloseElement);
-                } else if (listItem.length > 0) {
-                    $(buttonCloseElement).insertAfter(inputElement);
-                    $(`
-                        <ol class="list-group list-group-numbered list-group-item-click" style="
-                            position: absolute;
-                            background: white;
-                            width: 94%;
-                            box-sizing: border-box;
-                            height: 250px;
-                            overflow: scroll;
-                        ">${listItemString}</ol>
-                    `).insertAfter(inputElement);
-                } else {
-                    btnCloseAutoComplete.remove();
-                    listGroup.remove();
-                }
-            }
-            getDataAjax(data);
+//                 if (listGroup.length > 0) {
+//                     if (isScroll) {
+//                         listGroup.append(listItemString);
+//                     } else {
+//                         listGroup.html(listItemString);
+//                     }
+//                     btnCloseAutoComplete.replaceWith(buttonCloseElement);
+//                 } else if (listItem.length > 0) {
+//                     $(buttonCloseElement).insertAfter(inputElement);
+//                     $(`
+//                         <ol class="list-group list-group-numbered list-group-item-click" style="
+//                             position: absolute;
+//                             background: white;
+//                             width: 94%;
+//                             box-sizing: border-box;
+//                             height: 250px;
+//                             overflow: scroll;
+//                         ">${listItemString}</ol>
+//                     `).insertAfter(inputElement);
+//                 } else {
+//                     btnCloseAutoComplete.remove();
+//                     listGroup.remove();
+//                 }
+//             }
+//             getDataAjax(data);
 
-            // jika scroll nya maksimum
-            function handleScroll() {
-                function isScrollAtBottom(element) {
-                    return (element.scrollTop() + element.innerHeight() >= element[0].scrollHeight);
-                }
-                if (isScrollAtBottom($(this)) && !isMaxPage) {
-                    isScroll = true;
-                    $.ajax({
-                        url: `${baseurl}/autocomplete/barang`,
-                        type: 'get',
-                        dataType: 'json',
-                        data: {
-                            search: value,
-                            page: nextPage,
-                        },
-                        success: function (data) {
-                            getDataAjax(data);
-                        },
-                    })
-                }
-            }
+//             // jika scroll nya maksimum
+//             function handleScroll() {
+//                 function isScrollAtBottom(element) {
+//                     return (element.scrollTop() + element.innerHeight() >= element[0].scrollHeight);
+//                 }
+//                 if (isScrollAtBottom($(this)) && !isMaxPage) {
+//                     isScroll = true;
+//                     $.ajax({
+//                         url: `${baseurl}/autocomplete/barang`,
+//                         type: 'get',
+//                         dataType: 'json',
+//                         data: {
+//                             search: value,
+//                             page: nextPage,
+//                         },
+//                         success: function (data) {
+//                             getDataAjax(data);
+//                         },
+//                     })
+//                 }
+//             }
 
-            var olElement = document.querySelector('ol.list-group.list-group-numbered.list-group-item-click');
-            olElement.addEventListener('scroll', debounce(handleScroll, 500));
+//             var olElement = document.querySelector('ol.list-group.list-group-numbered.list-group-item-click');
+//             olElement.addEventListener('scroll', debounce(handleScroll, 500));
 
-            if (count_filtered == 0) {
-                resetAutocomplete();
-            }
-        }
-    })
+//             if (count_filtered == 0) {
+//                 resetAutocomplete();
+//             }
+//         }
+//     })
 
-}, 500));
+// }, 500));
 
 
 var resetAutocomplete = () => {
